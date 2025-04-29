9cbd93d52a99ff0e750dbcb6b239d647dbabd940
moso0x <mosokimothon@gmail.com>
2025-04-30 00:07:29 +0300

    added footer social icons

--- sync-frontend/app/components/Footer.tsx
+++ sync-frontend/app/components/Footer.tsx
@@ -67,28 +67,29 @@ export default function Footer() {
         <div className="mt-12 pt-12 border-t border-gray-700 flex flex-col md:flex-row justify-between items-center">
           <p>© 2025 KryptoSync. All rights reserved.</p>
           <div className="flex space-x-4 mt-4 md:mt-0">
+            
+            <a href="#" className="hover:text-green-300">
+            <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-facebook-icon lucide-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/>
+            </svg>
+            </a>
             <a href="https://x.com/crowdchainHQ" target="_blank" className="hover:text-green-300">
-              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              <svg className="w-5 h-5"  fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                 <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
               </svg>
             </a>
-            <a href="#" className="hover:text-green-300">
-              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                <path d="M20.317 4.369a19.791 19.791 0 00-4.885-1.515.074.074 0 00-.078.037c-.211.375-.444.864-.608 1.25-1.844-.276-3.68-.276-5.486 0a13.23 13.23 0 00-.617-1.25.077.077 0 00-.078-.037c-1.699.405-3.32.953-4.882 1.515a.07.07 0 00-.032.028C.533 9.263-.319 14.09.1 18.861a.082.082 0 00.031.056c2.052 1.507 4.053 2.422 6.005 3.027a.078.078 0 00.084-.028c.465-.635.877-1.303 1.226-2.011a.078.078 0 00-.041-.107 12.16 12.16 0 01-1.746-.835.078.078 0 01-.008-.13c.117-.087.234-.18.344-.276a.078.078 0 01.08-.01c3.674 1.677 7.658 1.677 11.303 0a.078.078 0 01.081.009c.11.097.227.19.344.277a.078.078 0 01-.007.13c-.556.338-1.14.631-1.747.834a.078.078 0 00-.04.108c.36.708.772 1.376 1.226 2.01a.078.078 0 00.084.028c1.962-.605 3.963-1.52 6.006-3.027a.078.078 0 00.031-.056c.5-5.178-.83-9.963-3.549-14.464a.06.06 0 00-.031-.028zM8.02 15.865c-1.18 0-2.147-1.08-2.147-2.41s.951-2.41 2.147-2.41c1.197 0 2.147 1.08 2.147 2.41s-.95 2.41-2.147 2.41zm7.974 0c-1.18 0-2.147-1.08-2.147-2.41s.951-2.41 2.147-2.41c1.197 0 2.147 1.08 2.147 2.41s-.95 2.41-2.147 2.41z" />
-              </svg>
-            </a>
             <a href="https://t.me/crowdchainHQ" target="_blank" className="hover:text-green-300">
-              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                <path d="M21.5 3.5L2.5 10.5C1.67 10.83 1.69 11.54 2.4 11.79L7.3 13.5L17.5 6.5C18 6.2 18.5 6.4 18.1 6.8L9 15.5V19C9 19.5 9.4 19.7 9.8 19.5L12.8 17.3L17 19.5C18 20 18.5 19.7 18.7 18.5L22 5.5C22.3 4.3 22 3.7 21.5 3.5Z"></path>
+            <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-instagram-icon lucide-instagram"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
+            </a>
+        
+            <a href="https://github.com/alexohre/crowdchain" target="_blank" className="hover:text-green-300">
+            <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-linkedin-icon lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/>
+      
               </svg>
             </a>
-            <a href="https://github.com/alexohre/crowdchain" target="_blank" className="hover:text-green-300">
-              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                <path
-                  fillRule="evenodd"
-                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                  clipRule="evenodd"
-                ></path>
+
+            <a  className="w-5 h-5" href="https://github.com/alexohre/crowdchain" target="_blank" className="hover:text-green-300">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-youtube-icon lucide-youtube"><path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"/><path d="m10 15 5-3-5-3z"/>
+          
               </svg>
             </a>
           </div>
